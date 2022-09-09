f68b4b99176344d0b1061f8a37d81d9366fe5007
kaneran <95705546+kaneran@users.noreply.github.com>
2022-09-09 20:21:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="box box1">Tokido</div>
+      <div className="box box2">Bonchan</div>
+      <div className="box box3">Ryusei</div>
+      <div className="box box4">Sako</div>
+    </div>
+  );
+}
